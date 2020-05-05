8b886ae14c55a2dc2cc99c5c491f9dc426a893a2
tmnkopp <timkopp@gmail.com>
2020-05-05 15:56:29 -0400

    Update snips.sql
    removed header

--- snips.sql
+++ snips.sql
@@ -1,4 +1,4 @@
-tg--snippet{"name":"cols from info schema", "category":"sql"}
+--snippet{"name":"cols from info schema", "category":"sql"}
 
 DECLARE @COLS  AS NVARCHAR(MAX)=''; 
 SELECT @COLS = @COLS + COLUMN_NAME + ',' FROM (SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='fsma_Activities' AND COLUMN_NAME NOT LIKE '%PK_%') vw
