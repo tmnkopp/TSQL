ad0bfd91246788503f39fa25f24aaf3816f97b0d
tmnkopp <timkopp@gmail.com>
2020-05-05 15:55:31 -0400

    Update snips.sql
    added loops

--- snips.sql
+++ snips.sql
@@ -1,4 +1,4 @@
---snippet{"name":"cols from info schema", "category":"sql"}
+tg--snippet{"name":"cols from info schema", "category":"sql"}
 
 DECLARE @COLS  AS NVARCHAR(MAX)=''; 
 SELECT @COLS = @COLS + COLUMN_NAME + ',' FROM (SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='fsma_Activities' AND COLUMN_NAME NOT LIKE '%PK_%') vw
