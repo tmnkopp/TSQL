6ac1bc504827d31a4d0ef7fc5aedf6b911279358
Tim Kopp <tim.kopp@cyber-balance.com>
2020-06-01 10:37:45 -0400

    prepop update

---
+++ PrePopulateDDForms.sql
@@ -0,0 +1,24 @@
+DECLARE @PK_FORM_TO NVARCHAR(15) = '2020-A-IG'  
+DECLARE @AG_ACRONYM NVARCHAR(5) = 'DOJ'   
+DECLARE @PK_FORM_TYPE NVARCHAR(15) = SUBSTRING( @PK_FORM_TO , 5, LEN(@PK_FORM_TO))  
+ 
+INSERT INTO fsma_Answers (Answer, FK_Question, FK_OrgSubmission ,DateModifed, Answer2 ) 
+SELECT MAX(ISNULL(Answer,'0')), QPKTO, ORG_TO.PK_OrgSubmission, GETDATE(), '[::]'
+FROM  vw_MetricsAnswers
+LEFT JOIN (
+	SELECT ID IDTO, QPK QPKTO FROM vw_MetricsAnswers WHERE PK_FORM = @PK_FORM_TO
+) FORM_TO ON FORM_TO.IDTO = vw_MetricsAnswers.ID
+CROSS JOIN (
+	SELECT PK_OrgSubmission FROM fsma_OrgSubmissions 
+	INNER JOIN fsma_ReportingCycle_Components ON fsma_ReportingCycle_Components.PK_ReportingCycle_Component = fsma_OrgSubmissions.FK_ReportingCycle_Component
+	INNER JOIN [Component List] CL ON fsma_ReportingCycle_Components.FK_Component=CL.PK_Component
+	WHERE PK_Form=@PK_FORM_TO	AND CL.Acronym = @AG_ACRONYM 
+) ORG_TO  
+WHERE ( PK_FORM LIKE '%19'+@PK_FORM_TYPE OR PK_FORM LIKE '%18'+@PK_FORM_TYPE  ) AND QPKTO IS NOT NULL
+GROUP BY  QPKTO, ORG_TO.PK_OrgSubmission
+
+SELECT * FROM fsma_Answers WHERE Answer2='[::]'
+
+/* 
+	DELETE FROM fsma_Answers WHERE Answer2='[::]' 
+*/
--- sp_DynamicUNPIV.sql
+++ sp_DynamicUNPIV.sql
@@ -1,19 +1,19 @@
 DECLARE @QUOTENAME  AS NVARCHAR(4000)=''; 
 DECLARE @FIELDS AS NVARCHAR(4000)=''; 
-DECLARE @TNAM  AS NVARCHAR(55)='fsma_Milestones'; 
+DECLARE @TNAM  AS NVARCHAR(55)='TIC_CloudServices'; 
 SELECT @QUOTENAME = @QUOTENAME + QUOTENAME(COLUMN_NAME) + ',' FROM (SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=@TNAM AND COLUMN_NAME NOT LIKE '%PK_%' AND COLUMN_NAME <> 'IsActive'  AND COLUMN_NAME <> 'DateCreated' AND COLUMN_NAME <> 'DateModified' AND COLUMN_NAME <> 'DateUpdated') vw
 SELECT @QUOTENAME = SUBSTRING(@QUOTENAME, 0, LEN(@QUOTENAME)) 
 SELECT @FIELDS =REPLACE( REPLACE(@QUOTENAME, '[', ''''), ']', '''')    
---SELECT @FIELDS 
+ SELECT @FIELDS 
 -- SELECT CHANGE_DATE, *  FROM AuditLog  WHERE TABLENAME = @TNAM  AND PK_PrimeKey=24 AND CHANGE_DATE > DATEADD(d,-15,GETDATE()) 
 -- UPDATE AuditLog SET CHANGE_DATE = DATEADD(d,-60,CHANGE_DATE) WHERE TABLENAME = 'fsma_Milestones' AND PK_PrimeKey=24 AND CHANGE_DATE > DATEADD(d,-15,GETDATE())
  
-SELECT TOP 0 PK_PrimeKey, Change_date, [Status],[AssignedTo],[Title],[TasksDesc],[ResourceReqmts],[MitConstraints],[ConExplanation],[SchCompDate],[ActCompDate],[Comments] FROM ( 
-	SELECT Change_date, FieldValue, FieldName, PK_PrimeKey FROM ( 
-		 SELECT FLATBYDATE.PK_AuditLog, Change_date, PK_PrimeKey, FieldName, FieldValue FROM ( 
-			SELECT MAX(PK_AuditLog) PK_AuditLog, Change_date, PK_PrimeKey, FieldName FROM ( 
+SELECT TOP 1000 PK_PrimeKey, Change_date, [Status],[AssignedTo],[Title],[TasksDesc],[ResourceReqmts],[MitConstraints],[ConExplanation],[SchCompDate],[ActCompDate],[Comments] FROM ( 
+	SELECT  y.FieldValue, Change_date,  FieldName, PK_PrimeKey FROM ( 
+		 SELECT FLATBYDATE.PK_AuditLog, VLOG.FieldValue , Change_date, PK_PrimeKey, FieldName FROM ( 
+			SELECT MAX(PK_AuditLog) PK_AuditLog, CONVERTDATE.Change_date, PK_PrimeKey, FieldName FROM ( 
 				SELECT  CONVERT(NVARCHAR(11), Change_date, 121)  Change_date,  PK_AuditLog , PK_PrimeKey, FieldName FROM AuditLog    
-			) ALOG GROUP BY Change_date,  PK_PrimeKey, FieldName  
+			) CONVERTDATE GROUP BY Change_date,  PK_PrimeKey, FieldName  
 		 ) FLATBYDATE
 		 INNER JOIN (
 			 SELECT PK_AuditLog, FieldValue FROM AuditLog WHERE TABLENAME = @TNAM AND FieldValue IS NOT NULL 
@@ -21,7 +21,7 @@ SELECT TOP 0 PK_PrimeKey, Change_date, [Status],[AssignedTo],[Title],[TasksDesc]
 	 ) y   
 ) x PIVOT( 
 	MAX(FieldValue) For FieldName IN ([Status],[AssignedTo],[Title],[TasksDesc],[ResourceReqmts],[MitConstraints],[ConExplanation],[SchCompDate],[ActCompDate],[Comments])
-) p  ORDER BY Change_date DESC
+) p  ORDER BY PK_PrimeKey DESC, Change_date DESC 
 
 DECLARE @exe NVARCHAR(MAX) = ''
 SET @exe = ' '  
@@ -29,7 +29,7 @@ SET @exe = @exe + ' SELECT  PK_PrimeKey, Change_date, '+ @QUOTENAME +' FROM (  '
 SET @exe = @exe + ' 	SELECT Change_date, FieldValue, FieldName, PK_PrimeKey FROM (  '
 SET @exe = @exe + ' 		 SELECT FLATBYDATE.PK_AuditLog, Change_date, PK_PrimeKey, FieldName, FieldValue FROM (  '
 SET @exe = @exe + ' 			SELECT MAX(PK_AuditLog) PK_AuditLog, Change_date, PK_PrimeKey, FieldName FROM (  '
-SET @exe = @exe + ' 				SELECT  CONVERT(NVARCHAR(11), Change_date, 121)  Change_date,  PK_AuditLog , PK_PrimeKey, FieldName FROM AuditLog     '
+SET @exe = @exe + ' 				SELECT  CONVERT(NVARCHAR(7), Change_date, 121)  Change_date,  PK_AuditLog , PK_PrimeKey, FieldName FROM AuditLog     '
 SET @exe = @exe + ' 			) ALOG GROUP BY Change_date,  PK_PrimeKey, FieldName   '
 SET @exe = @exe + ' 		 ) FLATBYDATE '
 SET @exe = @exe + ' 		 INNER JOIN ( '
@@ -41,4 +41,4 @@ SET @exe = @exe + ' 	MAX(FieldValue) For FieldName IN ('+ @QUOTENAME +') '
 SET @exe = @exe + ' ) p  ORDER BY PK_PrimeKey DESC, Change_date DESC '
 
 PRINT @exe 
- EXECUTE sp_executesql @exe 
+--EXECUTE sp_executesql @exe 
