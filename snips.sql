8a3a5c1e9e441442aa3a36e8b951512d91adb66d
Tim Kopp <tim.kopp@cyber-balance.com>
2020-09-25 10:55:29 -0400

    updated snip

--- snips.sql
+++ snips.sql
@@ -57,6 +57,22 @@ IF OBJECT_ID('tempdb..#MTRX') IS NOT NULL DROP TABLE #MTRX
 
 
 
+DECLARE @exe NVARCHAR(MAX) = ''
+SET @exe = ' '  
+SET @exe = @exe + ' SELECT  PK_PrimeKey, Change_date, '+ @QUOTENAME +' FROM (  '
+SET @exe = @exe + ' 	SELECT Change_date, FieldValue, FieldName, PK_PrimeKey FROM (  '
+SET @exe = @exe + ' 		 SELECT FLATBYDATE.PK_AuditLog, Change_date, PK_PrimeKey, FieldName, FieldValue FROM (  '
+SET @exe = @exe + ' 			SELECT MAX(PK_AuditLog) PK_AuditLog, Change_date, PK_PrimeKey, FieldName FROM (  '
+SET @exe = @exe + ' 				SELECT  CONVERT(NVARCHAR(7), Change_date, 121)  Change_date,  PK_AuditLog , PK_PrimeKey, FieldName FROM AuditLog     '
+SET @exe = @exe + ' 			) ALOG GROUP BY Change_date,  PK_PrimeKey, FieldName   '
+SET @exe = @exe + ' 		 ) FLATBYDATE '
+SET @exe = @exe + ' 		 INNER JOIN ( '
+SET @exe = @exe + ' 			 SELECT PK_AuditLog, FieldValue FROM AuditLog WHERE TABLENAME = '''+ @TNAM +''' AND FieldValue IS NOT NULL  '
+SET @exe = @exe + ' 		 ) VLOG ON VLOG.PK_AuditLog = FLATBYDATE.PK_AuditLog '
+SET @exe = @exe + ' 	 ) y    '
+SET @exe = @exe + ' ) x PIVOT(  '
+SET @exe = @exe + ' 	MAX(FieldValue) For FieldName IN ('+ @QUOTENAME +') '
+SET @exe = @exe + ' ) p  ORDER BY PK_PrimeKey DESC, Change_date DESC '
 
 
 
