34419de8a60a9447dccc1b69f32eed34aaaacfc9
Tim Kopp <timkopp@gmail.com>
2021-06-10 14:58:44 -0400

    add extended event script

--- XE_Session.sql
+++ XE_Session.sql
@@ -7,14 +7,20 @@ CREATE EVENT SESSION [Audit] ON SERVER
 	(
 		SET collect_data_stream=(0),collect_output_parameters=(1),collect_statement=(1)
 		ACTION(sqlserver.sql_text)
-		WHERE ([sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text],N'%SELECT%'))) 
-
+		WHERE (
+			[sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text],N'%Solar%') 
+		)
+	)  
 	, ADD EVENT sqlserver.sql_statement_completed
-    (
+    ( 
         ACTION(sqlserver.sql_text)
-        WHERE  ( [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%SELECT%')  )
+        WHERE ( 
+			[sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Solar%')
+			OR [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Artifact%')  
+			OR [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Audit%')  
+		)
     )
-ADD TARGET package0.event_file(SET filename=N'C:\temp\Audit.xel',max_file_size=(10))
+	ADD TARGET package0.event_file(SET filename=N'C:\temp\trace.xel',max_file_size=(10))
 GO
 ALTER EVENT SESSION [Audit] ON SERVER  STATE = start;  
 GO  
