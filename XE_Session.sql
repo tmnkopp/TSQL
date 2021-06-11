fd326893a04fb85dd2f5eaf006caffe89c00c36e
Tim Kopp <timkopp@gmail.com>
2021-06-11 10:55:00 -0400

    XE script update

--- XE_Session.sql
+++ XE_Session.sql
@@ -4,19 +4,18 @@ IF EXISTS(SELECT * FROM sys.server_event_sessions WHERE name='Audit')
 GO
 CREATE EVENT SESSION [Audit] ON SERVER 
 	ADD EVENT sqlserver.rpc_completed
-	(
-		SET collect_data_stream=(0),collect_output_parameters=(1),collect_statement=(1)
+	( 
 		ACTION(sqlserver.sql_text)
 		WHERE (
-			[sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text],N'%Solar%') 
+			   [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Artifact%')  
+			OR [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Audit%')  
 		)
 	)  
 	, ADD EVENT sqlserver.sql_statement_completed
     ( 
         ACTION(sqlserver.sql_text)
         WHERE ( 
-			[sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Solar%')
-			OR [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Artifact%')  
+			   [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Artifact%')  
 			OR [sqlserver].[like_i_sql_unicode_string]([sqlserver].[sql_text], N'%Audit%')  
 		)
     )
