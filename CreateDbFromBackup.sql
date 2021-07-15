3701d9d293eaf8ab6b0d7433ea76a3002c813ea6
Tim Kopp <timkopp@gmail.com>
2021-07-15 12:00:16 -0400

    update db backup

--- CreateDbFromBackup.sql
+++ CreateDbFromBackup.sql
@@ -9,31 +9,32 @@ END
 GO
 
 CREATE DATABASE [CyberScopeLite]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'CyberScopeLite', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.mdf' , SIZE = 1048576KB , FILEGROWTH = 262144KB )
- LOG ON 
-( NAME = N'CyberScopeLite_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite_log.ldf' , SIZE = 524288KB , FILEGROWTH = 131072KB )
+	 CONTAINMENT = NONE
+	 ON PRIMARY 
+	( NAME = N'CyberScopeLite', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.mdf' , SIZE = 1048576KB , FILEGROWTH = 262144KB )
+	 LOG ON 
+	( NAME = N'CyberScopeLite_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite_log.ldf' , SIZE = 524288KB , FILEGROWTH = 131072KB )
 GO  
-ALTER DATABASE [CyberScopeLite] SET RECOVERY SIMPLE WITH NO_WAIT
+	ALTER DATABASE [CyberScopeLite] SET RECOVERY SIMPLE WITH NO_WAIT
 GO 
-ALTER AUTHORIZATION ON DATABASE::[CyberScopeLite] TO [sa]
+	ALTER AUTHORIZATION ON DATABASE::[CyberScopeLite] TO [sa]
 GO
-ALTER DATABASE [CyberScopeLite] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-ALTER DATABASE [CyberScopeLite] SET MULTI_USER
+	ALTER DATABASE [CyberScopeLite] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	ALTER DATABASE [CyberScopeLite] SET MULTI_USER
 GO
 
 USE [master]
-RESTORE DATABASE [CyberScopeLite] FROM  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\Backup\CyberScope123.bak' WITH  FILE = 1, 
- MOVE N'CyberScorecard' TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.mdf', 
- MOVE N'CyberScorecard_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.ldf',  NORECOVERY,  NOUNLOAD,  REPLACE,  STATS = 5
- RESTORE DATABASE [CyberScopeLite] WITH RECOVERY
+RESTORE DATABASE [CyberScopeLite] FROM  
+	DISK = N'D:\Backup\CyberScope.bak' WITH  FILE = 1, 
+	MOVE N'CyberScorecard' TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.mdf', 
+	MOVE N'CyberScorecard_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\CyberScopeLite.ldf',  NORECOVERY,  NOUNLOAD,  REPLACE,  STATS = 5
+	RESTORE DATABASE [CyberScopeLite] WITH RECOVERY
 GO 
-ALTER DATABASE [CyberScopeLite] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-ALTER DATABASE [CyberScopeLite] SET MULTI_USER
+	ALTER DATABASE [CyberScopeLite] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	ALTER DATABASE [CyberScopeLite] SET MULTI_USER
 GO
 	SET NOCOUNT ON;
-	USE [CyberScope123] --  [CyberScopeLite] -- 
+	USE [CyberScopeLite] -- [CyberScope123] --  
 	IF OBJECT_ID('tempdb..#StmtProvider') IS NOT NULL DROP TABLE #StmtProvider 
 	CREATE TABLE #StmtProvider (ROWID INT IDENTITY (1, 1), STMT NVARCHAR(4000) )    
 	;WITH dbschema AS
@@ -59,11 +60,11 @@ GO
 	WHEN TableName LIKE '%BACKUP%'   THEN
 		'TRUNCATE TABLE [' +TableName+ ']'
 	WHEN TableName LIKE '%AuditLog%' THEN
-		'-- DELETE FROM [' +TableName+ '] WHERE [' + PK +  '] < (SELECT MAX(['+PK+']) - 10000 FROM [' +TableName+ '])'
+		' DELETE FROM [' +TableName+ '] WHERE [' + PK +  '] < (SELECT MAX(['+PK+']) - 10000 FROM [' +TableName+ '])'
 	WHEN TableName NOT LIKE 'fsma_%' AND TableName NOT LIKE 'wf_%'    THEN
 		'TRUNCATE TABLE [' +TableName+ ']' 
 	ELSE
-		'-- DELETE FROM [' +TableName+ '] WHERE [' + PK +  '] < (SELECT MAX(['+PK+']) - 10000 FROM [' +TableName+ '])'
+		' DELETE FROM [' +TableName+ '] WHERE [' + PK +  '] < (SELECT MAX(['+PK+']) - 10000 FROM [' +TableName+ '])'
 	END STMT FROM dbschema WHERE TotalSpaceMB > 4 
 	  
 	DECLARE @RowCnt INT = 1 
@@ -72,24 +73,24 @@ GO
 	BEGIN  
 		DECLARE @EXE NVARCHAR(MAX) = (SELECT ISNULL(STMT, '0') FROM #StmtProvider WHERE ROWID = @RowCnt) + ';'    
 		BEGIN TRY       
-			PRINT @EXE    --  EXECUTE sp_executesql @EXE             
+			EXECUTE sp_executesql @EXE  -- PRINT @EXE    --             
 		END TRY  
 		BEGIN CATCH   
 			PRINT ' err '+ @EXE
 		END CATCH   
 		SET @RowCnt = @RowCnt + 1 
-	END  
-	GO 
+	END   
+GO 
 
 USE [CyberScopeLite]
 GO
-DBCC SHRINKFILE (N'CyberScorecard_log' , 0, TRUNCATEONLY) 
+	DBCC SHRINKFILE (N'CyberScorecard_log' , 0, TRUNCATEONLY) 
 GO
-DBCC SHRINKDATABASE(N'CyberScopeLite' )
+	DBCC SHRINKDATABASE(N'CyberScopeLite' )
 GO 
-BACKUP DATABASE [CyberScopeLite] TO  DISK = N'D:\Backup\CyberScopeLite.bak' WITH NOFORMAT, NOINIT, 
-NAME = N'CyberScopeLite-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+	BACKUP DATABASE [CyberScopeLite] TO  DISK = N'D:\Backup\CyberScopeLite.bak' WITH NOFORMAT, NOINIT, 
+	NAME = N'CyberScopeLite-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
 GO
-BACKUP DATABASE [CyberScopeLite] TO  DISK = N'D:\Backup\CyberScopeLite.bak' WITH  DIFFERENTIAL ,NOFORMAT, NOINIT,  
-NAME = N'CyberScopeLite-DIFF Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+	BACKUP DATABASE [CyberScopeLite] TO  DISK = N'D:\Backup\CyberScopeLite.bak' WITH  DIFFERENTIAL ,NOFORMAT, NOINIT,  
+	NAME = N'CyberScopeLite-DIFF Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
 GO
